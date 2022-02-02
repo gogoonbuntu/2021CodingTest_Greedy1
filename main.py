4af43ceb362eacf47899bd92330802cbaff08bd4
Gogoonbuntu <34636395+gogoonbuntu@users.noreply.github.com>
2022-02-02 17:28:33 +0000

    실패로직 v0.1

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-def bundan (a):
-	return a**a
+def bundan ():
+	a = list(map(int, input().split()))
+	N = a[0]
+	K = a[1]
+	count = 0
+	while(N!=1):
+		mox = N%K
+		N -= mox
+		count += mox
+		div = N/K
+		count +=1
+		N = div
+		print("N=",N)
 
-print(bundan(10))
+	return count
+
+print(bundan())
+
+#N/K 가 나눗셈이 안될때 해결불가.
